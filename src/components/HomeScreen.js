ec208f069ed2f9f0881abe0e8e8be89f70dcd64a
mrinal1224 <iammrinalkc@gmail.com>
2021-08-07 23:16:10 +0530

    alignment issue fixed and ratings added

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,15 +1,11 @@
-import React , {useState , useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
-import { AppBar } from '@material-ui/core';
-import axios from 'axios'
-import ProductCard from './ProductCard';
-
-
-
-
-
+import { AppBar, Container } from "@material-ui/core";
+import axios from "axios";
+import {Grid} from "@material-ui/core"
+import ProductCard from "./ProductCard";
 
 const getStyles = makeStyles((theme) => ({
   main: {
@@ -24,98 +20,113 @@ const HomeScreen = () => {
   const [categories, setCategories] = useState([]);
   const [categoryID, setCategoryID] = useState("227");
   const [productList, setProductList] = useState([]);
-  const [catName , setCatName] = useState('Sale')
-  const [error , setError] =useState(false)
+  const [catName, setCatName] = useState("Sale");
+  const [error, setError] = useState(false);
 
-const onCategoryChange = (event, value) => {
+  const onCategoryChange = (event, value) => {
     setCategoryID(value);
   };
 
+  useEffect(async () => {
+    try {
+      const categoriesData = (
+        await axios.get(
+          "https://backend.ustraa.com/rest/V1/api/homemenucategories/v1.0.1?device_type=mob"
+        )
+      ).data;
+      setCategories(categoriesData["category_list"]);
+      console.log(categoriesData["category_list"]);
+    } catch (error) {
+      setError(true);
+    }
+  }, []);
 
- useEffect(async()=>{
-     try {
-          const categoriesData = (await axios.get(
-            "https://backend.ustraa.com/rest/V1/api/homemenucategories/v1.0.1?device_type=mob"
-          )).data;
-          setCategories(categoriesData.category_list)
-          console.log(categoriesData.category_list);
-     } catch (error) {
-        setError(true) 
-     }
-    
- },[])
-
-  useEffect(async() => {
-      try {
-           const individualCategory = (
-             await axios.get(
-               "https://backend.ustraa.com/rest/V1/api/catalog/v1.0.1?category_id=" +
-                 categoryID
-             )
-           ).data;
-           console.log(individualCategory);
-           setProductList(individualCategory)
-           
-           
-      } catch (error) {
-          setError(true)
-      }
-    
+  useEffect(async () => {
+    try {
+      const individualCategory = (
+        await axios.get(
+          "https://backend.ustraa.com/rest/V1/api/catalog/v1.0.1?category_id=" +
+            categoryID
+        )
+      ).data;
+      console.log(individualCategory["products"]);
+      setProductList(individualCategory["products"]);
+    } catch (error) {
+      setError(true);
+    }
   }, [categoryID]);
 
-  
+  if (error) {
+    return <h1>Error Occured Cannot Load Page</h1>;
+  } else {
+    return (
+      <div className={styles.main}>
+        <h1 style={{ marginLeft: "30%" }}>Our Products</h1>
+        <AppBar position="static" color="transparent">
+          <Tabs
+            value={categoryID}
+            indicatorColor="primary"
+            onChange={onCategoryChange}
+            textColor="white"
+            variant="scrollable"
+            scrollButtons="auto"
+            aria-label="scrollable auto tabs example"
+          >
+            {categories.map((category) => {
+              return (
+                <Tab
+                  key={category["category_id"]}
+                  value={category["category_id"]}
+                  label={category["category_name"]}
+                  style={{
+                    backgroundImage: `url(${category["category_image"]})`,
+                    backgroundSize: "100% 100%",
+                    marginTop: "5px",
+                    marginLeft: "8px",
+                    marginRight: "12px",
+                    borderRadius: "12px",
+                    marginBottom: "0.5rem",
+                    width: "120px",
+                    height: "65px",
+                    color: "#FFFFF0",
+                  }}
+                />
+              );
+            })}
+          </Tabs>
+        </AppBar>
 
-if(error){
-    return(
-        <h1>Error Occured Cannot Load Page</h1>
-    )
-}
+        <div style={{ marginTop: "50px" }}>
+          
+            <Grid
+              container
+              direction="column"
+              spacing={3}
+              xs={12}
+              sm={12}
+              md={12}
+            >
+              {productList.map((product) => {
+                return (
+                  <Grid key={product["id"]} item>
+                    <ProductCard
+                      pName={product["name"]}
+                      pImage={product["image_urls"]["x300"]}
+                      pWeight={[product["weight"]]}
+                      pWeightUnit={product["weight_unit"]}
+                      pSpecialPrice={product["final_price"]}
+                      pMrp={product["price"]}
+                      pRating={product["rating"]}
+                    />
+                  </Grid>
+                );
+              })}
+            </Grid>
+          
+        </div>
+      </div>
+    );
+  }
+};
 
-else{
-   return (
-     <div className={styles.main}>
-         <h1 style={{marginLeft:'30%'}}>Our Products</h1>
-       <AppBar position="static" color="transparent">
-         <Tabs
-           value={categoryID}
-           indicatorColor="primary"
-           onChange={onCategoryChange}
-           textColor="white"
-           variant="scrollable"
-           scrollButtons="auto"
-           aria-label="scrollable auto tabs example"
-         >
-           {categories &&
-             categories.map((category) => {
-               return (
-
-                 <Tab
-                   key={category["category_id"]}
-                   value={category["category_id"]}
-                   label={category["category_name"]}
-                   style={{
-                     backgroundImage: `url(${category["category_image"]})`,
-                     backgroundSize: "100% 100%",
-                     marginRight: "12px",
-                     borderRadius: '12px',
-                     marginBottom: "0.5rem",
-                     width: "120px",
-                     height: "65px",
-                     color:'white'
-                   }}
-                 />
-                 
-                 
-               );
-             })}
-         </Tabs>
-       </AppBar>
-       <ProductCard />
-     </div>
-   );
-}
-
- 
-}
-
-export default HomeScreen
+export default HomeScreen;
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from "react";
 
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import IconButton from "@material-ui/core/IconButton";
+import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-
+import { CardActions } from "@material-ui/core";
+import Rating from "./Rating";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
+    width: "100%",
   },
   details: {
     display: "flex",
@@ -20,42 +22,67 @@ const useStyles = makeStyles((theme) => ({
     flex: "1 0 auto",
   },
   cover: {
-    width: 151,
-  },
-  controls: {
-    display: "flex",
-    alignItems: "center",
-    paddingLeft: theme.spacing(1),
-    paddingBottom: theme.spacing(1),
-  },
-  playIcon: {
-    height: 38,
-    width: 38,
+    minWidth: "150px",
+    minHeight: "150px",
+    paddingRight: "20px",
   },
 }));
 
-const ProductCard = ({pName , pImage , pWeight , pWeightUnit, pMrp , pSpecialPrice , pRating , pInStock}) => {
-     const classes = useStyles();
-     const theme = useTheme();
-    return (
+const ProductCard = ({
+  pName,
+  pImage,
+  pWeight,
+  pWeightUnit,
+  pMrp,
+  pSpecialPrice,
+  pRating,
+  pInStock,
+}) => {
+  const classes = useStyles();
+
+  return (
+    <>
       <Card className={classes.root}>
+        <CardMedia className={classes.cover} image={pImage} />
         <div className={classes.details}>
           <CardContent className={classes.content}>
-            <Typography component="h5" variant="h5">
-              Live From Space
+            <Typography component="h6" variant="h6" style={{ fontSize: 15 }}>
+              {pName}
             </Typography>
-            <Typography variant="subtitle1" color="textSecondary">
-              Mac Miller
+            <Typography
+              variant="subtitle2"
+              style={{ color: "#696969", fontSize: 12 }}
+            >
+              ({pWeight}
+              {pWeightUnit})
             </Typography>
+
+            <Rating value={pRating} text={pRating} />
+            <Typography
+              variant="subtitle2"
+              style={{ color: "#4BB543", fontWeight: 700, fontSize: 16 , marginTop:'5px' }}
+            >
+              Rs. {pSpecialPrice} <strike style={{ color: "#696969", fontSize: 12 }}>
+                  Rs. {pMrp}
+              </strike>
+            </Typography>
+            <Button
+              variant="contained"
+              style={{
+                position: "sticky",
+                backgroundColor: "#4BB543",
+                color: "white",
+                marginTop: "20px",
+                marginRight: "10px",
+              }}
+            >
+              Add to Cart
+            </Button>
           </CardContent>
         </div>
-        <CardMedia
-          className={classes.cover}
-          image="/static/images/cards/live-from-space.jpg"
-          title="Live from space album cover"
-        />
       </Card>
-    );
-}
+    </>
+  );
+};
 
-export default ProductCard
+export default ProductCard;
